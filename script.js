2d4277ff8186658597ac9b16773bc740a7f0d3b1
Aryan Bista <aryanbista125@gmail.com>
2026-09-08 08:22:45 +0545

    Wk-2 Work

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let button = document.getElementById("addButton");
-
-button.addEventListener("click", function() {
-
+function sum(){
     let number1 = Number(document.getElementById("number1").value);
     let number2 = Number(document.getElementById("number2").value);
 
     let answer = number1 + number2;
 
     document.getElementById("result").innerHTML = answer;
-});
+}
+
+function subtract(){
+
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+
+    let answer = number1 - number2;
+
+    document.getElementById("result").innerHTML = answer;
+
+}
+
+function multiply(){
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+
+    let answer = number1 * number2;
+
+    document.getElementById("result").innerHTML = answer;
+
+}
