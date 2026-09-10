0e96174f98762bd7ab069fd7ad50ce3cf0780606
Aryan Bista <aryanbista125@gmail.com>
2026-09-10 07:39:03 +0545

    calc

---
+++ calc.js
@@ -0,0 +1,15 @@
+
+function calculate(value) {
+
+    let display = document.getElementById("display");
+
+    if (value == "C") {
+        display.value = "";
+    }
+    else if (value == "=") {
+        display.value = eval(display.value);
+    }
+    else {
+        display.value = display.value + value;
+    }
+}
